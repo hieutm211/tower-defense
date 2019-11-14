64ecf510d4c4c70558dba51ae1326a724d9a602f
Giang Bang <banggiangle2015@gmail.com>
2019-11-15 00:12:00 +0700

    make curved route

--- app/src/main/java/com/ligrim/tower_defense/Position.java
+++ app/src/main/java/com/ligrim/tower_defense/Position.java
@@ -24,8 +24,58 @@ public class Position {
         return x;
     }
 
+    public static Position add(Position first, Position second) {
+        return new Position(first.getX() + second.getX(), first.getY() + second.getY());
+    }
+
+    public static float distanceSquared(Position first, Position second) {
+        float dx = first.getX() - second.getX();
+        float dy = first.getY() - second.getY();
+        return dx * dx + dy * dy;
+    }
+
+    public static Position midPoint(Position first, Position second) {
+        return new Position((first.x + second.x) / 2, (first.y + second.y) / 2);
+    }
+
     public float getY() {
         return y;
+    }
+
+    public static float cross(Position u, Position v) {
+        return u.x * v.y - v.x * u.y;
+    }
+
+    // return absolute angle in radian, always a minimum positive float
+    public static float getAbsoluteAngle(Position vector) {
+
+        double tan = vector.y / vector.x;
+        float arctan = (float) Math.atan(tan);
+        float pi = (float) Math.PI;
+
+        if (Math.abs(vector.x) < 0.0001f) {
+            if (vector.y > 0f) return pi / 2;
+            return 3 * pi / 2;
+        }
+        else if (Math.abs(vector.y) < 0.0001f ) {
+            if (vector.x > 0f) return 0f;
+            else return pi;
+        }
+        else if (vector.y > 0f && vector.x > 0f) return Math.abs(arctan);
+        else if (vector.y > 0f && vector.x < 0f) return arctan + pi;
+        else if (vector.y < 0f && vector.x < 0f) return arctan + pi;
+        else return arctan + 2 * pi;
+    }
+
+    public static float getAngleBetween(Position a, Position b) {
+        float da = distance(new Position(0, 0), a);
+        float db = distance(new Position(0, 0), b);
+        float angle = (float) Math.acos(da*db/dot(a, b));
+        return  angle;
+    }
+
+    public static float dot(Position a, Position b) {
+        return a.x * b.x + a.y * b.y;
     }
 
     public static float distance(Position first, Position second) {
