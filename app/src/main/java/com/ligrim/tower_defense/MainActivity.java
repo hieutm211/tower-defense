430cd1a83df050307810aa48fbf1c81c92b6c467
Hieu Tran <hieutm211@gmail.com>
2019-11-21 17:59:58 +0700

    add 2 more maps to starting screen

--- app/src/main/java/com/ligrim/tower_defense/MainActivity.java
+++ app/src/main/java/com/ligrim/tower_defense/MainActivity.java
@@ -36,14 +36,35 @@ public class MainActivity extends Activity {
 
         Button map_1 = findViewById(R.id.map_1);
         map_1.setVisibility(View.VISIBLE);
-        map_1.animate().alpha(1f).setDuration(1000);
+        map_1.animate().alpha(1f).setDuration(2000);
+
+        Button map_2 = findViewById(R.id.map_2);
+        map_2.setVisibility(View.VISIBLE);
+        map_2.animate().alpha(1f).setDuration(2000);
+
+        Button map_3 = findViewById(R.id.map_3);
+        map_3.setVisibility(View.VISIBLE);
+        map_3.animate().alpha(1f).setDuration(2000);
     }
 
     public void enterGame(View view) {
+        String mapId = null;
+        switch (view.getId()) {
+            case R.id.map_1:
+                mapId = "map_1";
+                break;
+            case R.id.map_2:
+                mapId = "map_2";
+                break;
+            case R.id.map_3:
+                mapId = "map_3";
+                break;
+        }
+
         try {
-            InputStream mapFile = getAssets().open("map/map_3/tilemap_info.tmx");
-            InputStream enemyFile = getAssets().open("map/map_3/enemy_info.txt");
-            InputStream saveFile = getAssets().open("map/map_2/saveFile.xml");
+            InputStream mapFile = getAssets().open("map/" + mapId + "/tilemap_info.tmx");
+            InputStream enemyFile = getAssets().open("map/" + mapId + "/enemy_info.txt");
+            InputStream saveFile = getAssets().open("map/" + mapId + "/saveFile.xml");
             GameStage gameStage = new GameStage(mapFile, enemyFile, saveFile);
             GameField gameField = new GameField(this, gameStage);
             setContentView(gameField);
